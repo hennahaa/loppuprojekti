aad437594c8b3892495b8d6f45dca663a0043246
Tiina <koodiit@gmail.com>
2022-01-03 16:31:16 +0200

    Funktio luo signed urlin, ottaa html-dataa.

--- app/tokeninluojafunktio/main.py
+++ app/tokeninluojafunktio/main.py
@@ -1,39 +1,48 @@
-#TO DO: vastaanottajan meilin hakeminen (rivi 20), miten, mistä, häh?
-#TO DO: tietokantaconffaus (rivi 25), haetaan Secret Managerista
+#TO DO: tietokantayhteyskonffaus
 
 import string, random
 import psycopg2
+from google.cloud import storage
+import datetime
 
-def luo_token_ja_url(event, context):
-    """Lisää tietokantaan e-korttikuvatiedoston bucket-url:in ja satunnaisen 15-merkkisen tokenin
+def event_tietokantaan(event):
+    """HTTPS-funktio, joka käsittelee frontin lomakkeesta tulevaa dataa ja lisää tietokantaan.
     Args:
         event (dict):  The dictionary with data specific to this type of event.
                        The `data` field contains a description of the event in
                        the Cloud Storage `object` format described here:
-                       https://cloud.google.com/storage/docs/json_api/v1/objects#resource
-        context (google.cloud.functions.Context): Metadata of triggering event."""
+                       https://cloud.google.com/storage/docs/json_api/v1/objects#resource"""
 
     token = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
     bucket_name = event['bucket']
     object_name = event['name']
-    object_url = 'https://storage.cloud.google.com/{}/{}'.format(bucket_name,object_name)
-    vastaanottajan_meili = ''
+    vastaanottajan_meili = event['email']
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(object_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # URL on voimiassa 7 vrk
+        expiration=datetime.timedelta(seconds=604800),
+        # sallitaan GET requestit
+        method="GET",
+    )
 
     # otetaan yhteys tietokantaan
     con = None
     try:
         con = psycopg2.connect('dbname= user= password= host=')
-        return con
     except (Exception,psycopg2.DatabaseError) as error:
         print(error, "Could not connect to database.")
 
     cursor = con.cursor()
-    SQL = '''INSERT INTO #tablename() VALUES (%s,%s,%s);'''
-    cursor.execute(SQL, object_url, token, vastaanottajan_meili)
+    SQL = '''INSERT INTO #tablename VALUES (%s,%s,%s);'''
+    cursor.execute(SQL, url, token, vastaanottajan_meili)
     con.commit()
-    print(f"Tietokanta päivitetty.")
+    print("Tietokanta päivitetty.")
     cursor.close()
     
     if con is not None:
-            con.close()    
-        
+            con.close()
