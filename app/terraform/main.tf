dd969c98dd2441a868b6ffa017a88b9e396eaf94
Henna <henna.haapala@gmail.com>
2022-01-04 09:47:39 +0200

    lisää terraform runkoa

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -14,3 +14,22 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
+#Placeholder Cloud Run
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
