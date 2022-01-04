c54bead267bb6f4a1fc31657aebdea4c2f8bb232
Tiina <koodiit@gmail.com>
2022-01-04 15:16:30 +0200

    Mol. funktiot hakevat salaisuudet Secretsistä

--- app/deletefunktio/main.py
+++ app/deletefunktio/main.py
@@ -1,14 +1,35 @@
 # TO DO: tietokannan speksit haettava salaisuuksista kovakoodauksen sijasta
 
 import psycopg2
-#from google.cloud import secretmanager
-import json
+from google.cloud import secretmanager
+import json, os
     
 def poistatoken(request):
-    """Poistaa e-korttitokenin tietokannasta, trigger JavaScriptistä API Gatewayhin frontista"""
+    """Poistaa e-korttitokenin tietokannasta, trigger frontin JavaScriptistä"""
+    
+    # haetaan Secret Managerista tietokannan speksit, ympäristömuuttujasta GCP project ID
+    client = secretmanager.SecretManagerServiceClient()
+    
+    project_id = os.environ.get('PROJECTID')
+    
+    path_dbname = "projects/{}/secrets/{}/versions/{}".format(project_id, 'kekkoslovakia-db-name', 'latest')
+    encr_dbname = client.access_secret_version(request={"name": path_dbname})
+    dbname = encr_dbname.payload.data.decode("UTF-8")
+
+    path_username = "projects/{}/secrets/{}/versions/{}".format(project_id, 'kekkoslovakia-db-user', 'latest')
+    encr_user = client.access_secret_version(request={"name": path_username})
+    user = encr_user.payload.data.decode("UTF-8")
+    
+    path_password = "projects/{}/secrets/{}/versions/{}".format(project_id, 'kekkoslovakia-db-srv-pw', 'latest')    
+    encr_password = client.access_secret_version(request={"name": path_password})
+    password = encr_password.payload.data.decode("UTF-8")
+
+    host = '34.88.169.103' # host url on aina sama, joten olkoon julkinen
+    
+    # otetaan yhteys tietokantaan, poistetaan triggeristä saatu token-arvo tietokannasta
     con = None
     try:
-        con = psycopg2.connect(database="", user="", password="", host="")
+        con = psycopg2.connect(database=f"{dbname}", user=f"{user}", password=f"{password}", host=f"{host}")
         cursor = con.cursor()
 
         request_json = request.get_json(silent=True)
