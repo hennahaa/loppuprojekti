c0d541a7c4df6d21d1b1cf995ebe6020bb552ffc
jajuhir <jhirvioja@gmail.com>
2022-01-10 09:57:58 +0200

    0.2 versio ennen cloud runiin laittamista, lisätty loggauksia ja poistettu yksi nappi

--- frontend/secretsconfig.py
+++ frontend/secretsconfig.py
@@ -1,8 +1,14 @@
 from google.cloud import secretmanager
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 def access_secret_version(project_id, secret_id, version_id):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(request={"name": name})
-    payload = response.payload.data.decode("UTF-8")
-    return payload
+    try:
+        client = secretmanager.SecretManagerServiceClient()
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+        response = client.access_secret_version(request={"name": name})
+        payload = response.payload.data.decode("UTF-8")
+        return payload
+    except RuntimeError as err:
+        print(err)
+        return logging.error(f"Secretsconfigin access_secret_version failasi (ongelma yhteydessä GCP:hen)")
